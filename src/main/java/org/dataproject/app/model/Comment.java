6bd3b35021a0081130a53561596580292910ca40
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-30 21:40:55 +0200

    create Comment model

---
+++ src/main/java/org/dataproject/app/model/Comment.java
@@ -0,0 +1,23 @@
+package org.dataproject.app.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name="commentaire")
+@Data
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="commentaire_id")
+	private int commentId;
+	
+	@Column(name="contenu")
+	private String content;
+}
