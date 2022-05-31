f1949f2fbbce6cb0aee99fb7599a85b5f821c957
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-31 19:48:56 +0200

    Update ProductService.java

--- src/main/java/org/dataproject/app/service/ProductService.java
+++ src/main/java/org/dataproject/app/service/ProductService.java
@@ -1,8 +1,22 @@
 package org.dataproject.app.service;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.dataproject.app.model.Product;
+import org.dataproject.app.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
-
+	@Autowired
+	private ProductRepository pr;
+	
+	public ArrayList<Product> getAllProducts() {
+		return (ArrayList<Product>) pr.findAll();
+	}
+	public Optional<Product> getProductById(Integer id) {
+		return pr.findById(id);
+	}
 }
