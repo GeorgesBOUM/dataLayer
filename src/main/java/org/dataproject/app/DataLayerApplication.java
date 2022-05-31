cdc6748ab1c6b813fea5205ae6db17bbbfd4b183
BOUM Georges <37276156+GeorgesBOUM@users.noreply.github.com>
2022-05-31 19:48:14 +0200

    Update DataLayerApplication.java

--- src/main/java/org/dataproject/app/DataLayerApplication.java
+++ src/main/java/org/dataproject/app/DataLayerApplication.java
@@ -1,13 +1,29 @@
 package org.dataproject.app;
 
+import java.util.ArrayList;
+
+import org.dataproject.app.model.Product;
+import org.dataproject.app.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataLayerApplication {
+public class DataLayerApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ProductService ps;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataLayerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ArrayList<Product> allProducts = ps.getAllProducts();
+		allProducts.forEach(product -> System.out.println(product.getName()));
+		
 	}
 
 }
